c508e86202d9d2b225b22ce0f4f451d52c22aeca
Brad Eric Hollister <brad.hollister@gmail.com>
2015-06-12 16:34:08 -0700

    added preliminary nmg_msv CLI command. command accepts model name and adds shell / vertex to the first region encountered via BU_LIST_FIRST. the command also performs NMG_CK_REGION. if a model doesn't have a region, then this check causes MGED to log a bad pointer error and crash. TODO: may need to find a way to specify region to CLI and handle error w/o crash.

--- src/libged/nmg_msv.c
+++ src/libged/nmg_msv.c
@@ -35,7 +35,57 @@
 int
 ged_nmg_msv(struct ged *gedp, int argc, const char *argv[])
 {
-	return GED_ERROR;
+	/* Make Shell, Vertex. Creates a new shell consisting of a single vertex in
+	 * the parameter region. A new vertex is created for the shell.
+	 */
+
+	struct rt_db_internal nmg_intern;
+	struct directory *dp;
+	const char *nmg_name;
+	struct model *m;
+	struct nmgregion *r;
+
+	static const char *usage = "nmg_name";
+
+	GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
+	GED_CHECK_READ_ONLY(gedp, GED_ERROR);
+	GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
+
+	if (argc != 2) {
+    bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+    return GED_HELP;
+    }
+
+	/* attempt to resolve and verify before we jump in */
+	nmg_name = argv[1];
+
+	if ((dp=db_lookup(gedp->ged_wdbp->dbip, nmg_name, LOOKUP_QUIET)) == RT_DIR_NULL) {
+    bu_vls_printf(gedp->ged_result_str, "%s does not exist\n", nmg_name);
+    return GED_ERROR;
+	}
+
+	if (rt_db_get_internal(&nmg_intern, dp, gedp->ged_wdbp->dbip, bn_mat_identity, &rt_uniresource) < 0) {
+    bu_vls_printf(gedp->ged_result_str, "rt_db_get_internal() error\n");
+    return GED_ERROR;
+	}
+
+	if (nmg_intern.idb_type != ID_NMG) {
+    bu_vls_printf(gedp->ged_result_str, "%s is not an NMG solid\n", nmg_name);
+    rt_db_free_internal(&nmg_intern);
+    return GED_ERROR;
+	}
+
+	m = (struct model *)nmg_intern.idb_ptr;
+	NMG_CK_MODEL(m);
+
+	/* for now, let's just added shell and vertex for the first
+	 * encountered region in model.
+	 * TODO: take region from CLI */
+	r = BU_LIST_FIRST(nmgregion, &m->r_hd);
+	NMG_CK_REGION(r)
+	nmg_msv(r);
+
+	return GED_OK;
 }
 
 /*
