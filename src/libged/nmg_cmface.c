74902ee6b8c2737b625e81ae26c9193ff8ebbf92
Brad Eric Hollister <brad.hollister@gmail.com>
2015-07-10 17:59:37 -0700

    using method from rt_nmg_adjust. still some issues with calls to nmg_cmface. soon to be resolved though.

--- src/libged/nmg_cmface.c
+++ src/libged/nmg_cmface.c
@@ -34,7 +34,7 @@
 
 #include "./ged_private.h"
 
-#define VERTNUM 4
+#define VERTNUM 3
 
 int
 ged_nmg_cmface(struct ged *gedp, int argc, const char *argv[])
@@ -48,16 +48,18 @@ ged_nmg_cmface(struct ged *gedp, int argc, const char *argv[])
     struct nmgregion* r;
     struct shell* s;
     struct vertex *verts[VERTNUM];
-    struct vertex **pverts[VERTNUM];
     struct faceuse *fu;
     struct bn_tol tol;
     int idx;
     struct bu_list* lst;
+    struct vertex ***face_verts;
+    int len;
+    int num_verts = VERTNUM;
+    int i, j;
+    fastf_t *tmp;
 
     static const char *usage = "nmg_name x0 y0 z0 x1 y1 z1 x2 y2 z2";
-/*
-    size = sizeof(struct vertex);
-*/
+
     GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
     GED_CHECK_READ_ONLY(gedp, GED_ERROR);
     GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
@@ -91,21 +93,16 @@ ged_nmg_cmface(struct ged *gedp, int argc, const char *argv[])
     m = (struct model *)internal.idb_ptr;
     NMG_CK_MODEL(m);
 
-    /* For now, let's just add quad face to the first
-    *  region's first shell.
-    *  TODO: take shell from CLI
-    */
-    r = BU_LIST_FIRST(nmgregion, &m->r_hd);
+    if (BU_LIST_IS_EMPTY(&m->r_hd)) {
+        r = nmg_mrsv(m);
+        s = BU_LIST_FIRST(shell, &r->s_hd);
+    } else {
+        r = BU_LIST_FIRST(nmgregion, &m->r_hd);
+        s = BU_LIST_FIRST(shell, &r->s_hd);
+    }
+
     NMG_CK_REGION(r);
-
-    s = BU_LIST_FIRST(shell, &r->s_hd);
     NMG_CK_SHELL(s);
-
-    tol.magic = BN_TOL_MAGIC;
-    tol.dist = 0.01;
-    tol.dist_sq = tol.dist * tol.dist;
-    tol.perp = 0.001;
-    tol.para = 0.999;
 
     for(idx = 0; idx < VERTNUM; idx++) {
         GET_VERTEX(verts[idx], m)
@@ -122,8 +119,12 @@ ged_nmg_cmface(struct ged *gedp, int argc, const char *argv[])
         vg->magic = NMG_VERTEX_G_MAGIC;
         verts[idx]->vg_p = vg;
         verts[idx]->magic = NMG_VERTEX_MAGIC;
-        pverts[idx] = &verts[idx];
     }
+
+    face_verts = (struct vertex ***) bu_calloc( VERTNUM,
+            sizeof(struct vertex **), "face_verts");
+    fu = nmg_cmface( s, face_verts, VERTNUM );
+    bu_free((char *) face_verts, "face_verts");
 
     for (idx=0; idx < VERTNUM*3; idx+=3){
         nmg_vertex_g(verts[idx/3], (fastf_t)atof(argv[idx+2]),
@@ -131,13 +132,182 @@ ged_nmg_cmface(struct ged *gedp, int argc, const char *argv[])
                                    (fastf_t)atof(argv[idx+4]));
     }
 
-    fu = nmg_cmface(s, &pverts[0], VERTNUM);
-    (void)nmg_fu_planeeqn(fu, &tol);
+    for (j=0; j<VERTNUM; j++) {
+       face_verts[j] = &verts[j];
+    }
 
-    NMG_CK_MODEL(m);
+    /* assign face geometry */
+    if (s) {
+        for (BU_LIST_FOR (fu, faceuse, &s->fu_hd)) {
+            if (fu->orientation != OT_SAME)
+                continue;
+            nmg_calc_face_g(fu);
+        }
+    }
+
+    tol.magic = BN_TOL_MAGIC;
+    tol.dist = 0.0005;
+    tol.dist_sq = tol.dist * tol.dist;
+    tol.perp = 1e-6;
+    tol.para = 1 - tol.perp;
+
+    nmg_rebound(m, &tol);
 
     return GED_OK;
 }
+
+#if 0
+int
+rt_nmg_adjust(struct bu_vls *logstr, struct rt_db_internal *intern, int argc, const char **argv)
+{
+    struct model *m;
+    struct nmgregion *r=NULL;
+    struct shell *s=NULL;
+    struct faceuse *fu=NULL;
+    Tcl_Obj *obj, **obj_array;
+    int len;
+    int num_verts = 0;
+    int num_loops = 0;
+    int *loop;
+    int loop_len;
+    int i, j;
+    struct tmp_v *verts;
+    fastf_t *tmp;
+    struct bn_tol tol;
+
+    RT_CK_DB_INTERNAL(intern);
+    m = (struct model *)intern->idb_ptr;
+    NMG_CK_MODEL(m);
+
+    verts = (struct tmp_v *)NULL;
+    for (i=0; i<argc; i += 2) {
+    if (BU_STR_EQUAL(argv[i], "V")) {
+        obj = Tcl_NewStringObj(argv[i+1], -1);
+        if (Tcl_ListObjGetElements(brlcad_interp, obj, &num_verts,
+                       &obj_array) != TCL_OK) {
+        bu_vls_printf(logstr,
+                  "ERROR: failed to parse vertex list\n");
+        Tcl_DecrRefCount(obj);
+        return BRLCAD_ERROR;
+        }
+        verts = (struct tmp_v *)bu_calloc(num_verts,
+                          sizeof(struct tmp_v),
+                          "verts");
+        for (j=0; j<num_verts; j++) {
+        len = 3;
+        tmp = &verts[j].pt[0];
+        if (tcl_obj_to_fastf_array(brlcad_interp, obj_array[j],
+                       &tmp, &len) != 3) {
+            bu_vls_printf(logstr,
+                  "ERROR: incorrect number of coordinates for vertex\n");
+            return BRLCAD_ERROR;
+        }
+        }
+
+    }
+    }
+
+    while (argc >= 2) {
+    struct vertex ***face_verts;
+
+    if (BU_STR_EQUAL(argv[0], "V")) {
+        /* vertex list handled above */
+        goto cont;
+    } else if (BU_STR_EQUAL(argv[0], "F")) {
+        if (!verts) {
+        bu_vls_printf(logstr,
+                  "ERROR: cannot set faces without vertices\n");
+        return BRLCAD_ERROR;
+        }
+        if (BU_LIST_IS_EMPTY(&m->r_hd)) {
+        r = nmg_mrsv(m);
+        s = BU_LIST_FIRST(shell, &r->s_hd);
+        } else {
+        r = BU_LIST_FIRST(nmgregion, &m->r_hd);
+        s = BU_LIST_FIRST(shell, &r->s_hd);
+        }
+        obj = Tcl_NewStringObj(argv[1], -1);
+        if (Tcl_ListObjGetElements(brlcad_interp, obj, &num_loops,
+                       &obj_array) != TCL_OK) {
+        bu_vls_printf(logstr,
+                  "ERROR: failed to parse face list\n");
+        Tcl_DecrRefCount(obj);
+        return BRLCAD_ERROR;
+        }
+        for (i=0, fu=NULL; i<num_loops; i++) {
+        struct vertex **loop_verts;
+        /* struct faceuse fu is initialized in earlier scope */
+
+        loop_len = 0;
+        (void)tcl_obj_to_int_array(brlcad_interp, obj_array[i],
+                       &loop, &loop_len);
+        if (!loop_len) {
+            bu_vls_printf(logstr,
+                  "ERROR: unable to parse face list\n");
+            return BRLCAD_ERROR;
+        }
+        if (i) {
+            loop_verts = (struct vertex **)bu_calloc(
+            loop_len,
+            sizeof(struct vertex *),
+            "loop_verts");
+            for (i=0; i<loop_len; i++) {
+            loop_verts[i] = verts[loop[i]].v;
+            }
+            fu = nmg_add_loop_to_face(s, fu,
+                          loop_verts, loop_len,
+                          OT_OPPOSITE);
+            for (i=0; i<loop_len; i++) {
+            verts[loop[i]].v = loop_verts[i];
+            }
+        } else {
+            face_verts = (struct vertex ***)bu_calloc(
+            loop_len,
+            sizeof(struct vertex **),
+            "face_verts");
+            for (j=0; j<loop_len; j++) {
+            face_verts[j] = &verts[loop[j]].v;
+            }
+            fu = nmg_cmface(s, face_verts, loop_len);
+            bu_free((char *)face_verts, "face_verts");
+        }
+        }
+    } else {
+        bu_vls_printf(logstr,
+              "ERROR: Unrecognized parameter, must be V or F\n");
+        return BRLCAD_ERROR;
+    }
+    cont:
+    argc -= 2;
+    argv += 2;
+    }
+
+    /* assign geometry for entire vertex list (if we have one) */
+    for (i=0; i<num_verts; i++) {
+    if (verts[i].v)
+        nmg_vertex_gv(verts[i].v, verts[i].pt);
+    }
+
+    /* assign face geometry */
+    if (s) {
+    for (BU_LIST_FOR (fu, faceuse, &s->fu_hd)) {
+        if (fu->orientation != OT_SAME)
+        continue;
+        nmg_calc_face_g(fu);
+    }
+    }
+
+    tol.magic = BN_TOL_MAGIC;
+    tol.dist = 0.0005;
+    tol.dist_sq = tol.dist * tol.dist;
+    tol.perp = 1e-6;
+    tol.para = 1 - tol.perp;
+
+    nmg_rebound(m, &tol);
+
+    return BRLCAD_OK;
+}
+#endif
 
 /*
  * Local Variables:
@@ -148,3 +318,4 @@ ged_nmg_cmface(struct ged *gedp, int argc, const char *argv[])
  * End:
  * ex: shiftwidth=4 tabstop=8
  */
+
